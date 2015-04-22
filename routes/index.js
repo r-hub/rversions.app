51822b7a655e21d48b6e8938300cb059468d17c3
Gabor Csardi <csardi.gabor@gmail.com>
2015-04-22 17:02:19 -0400

    First working version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.end('{ "info": "https://github.com/metacran/rversions.app" }');
+});
+
+router.get('/r-release', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-release.json")
+});
+
+router.get('/r-oldrel', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-oldrel.json")
+});
+
+router.get('/r-versions', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-versions.json")
 });
 
 module.exports = router;
