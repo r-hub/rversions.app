d71b3a0ff1a4bd3df81740cde0d6242cfbd123bf
Gábor Csárdi <csardi.gabor@gmail.com>
2016-10-31 14:27:25 +0000

    Various r-release downloads, closes #2
    /r-release-tarball
    /r-release-win
    /r-release-macos

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,22 @@ router.get('/r-versions', function(req, res) {
     res.sendfile("./r-versions.json")
 });
 
+router.get('/r-release-tarball', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-release-tarball.json")
+});
+
+router.get('/r-release-win', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-release-win.json")
+});
+
+router.get('/r-release-macos', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set(200)
+    res.sendfile("./r-release-macos.json")
+});
+
 module.exports = router;
