fd72cac224e5132b5294ff1a5b8f21c10296018e
Gábor Csárdi <csardi.gabor@gmail.com>
2017-10-02 10:50:43 +0100

    Use sendFile, sendfile is deprecated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
+
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res) {
     res.set('Content-Type', 'application/json');
@@ -10,37 +12,40 @@ router.get('/', function(req, res) {
 router.get('/r-release', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-release.json")
+    res.sendFile("./r-release.json", { root: path.join(__dirname, "..") })
 });
 
 router.get('/r-oldrel', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-oldrel.json")
+    res.sendFile("./r-oldrel.json", { root: path.join(__dirname, "..") })
 });
 
 router.get('/r-versions', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-versions.json")
+    res.sendFile("./r-versions.json", { root: path.join(__dirname, "..") })
 });
 
 router.get('/r-release-tarball', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-release-tarball.json")
+    res.sendFile("./r-release-tarball.json",
+		 { root: path.join(__dirname, "..") })
 });
 
 router.get('/r-release-win', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-release-win.json")
+    res.sendFile("./r-release-win.json",
+		 { root: path.join(__dirname, "..") })
 });
 
 router.get('/r-release-macos', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.set(200)
-    res.sendfile("./r-release-macos.json")
+    res.sendFile("./r-release-macos.json",
+		 { root: path.join(__dirname, "..") })
 });
 
 module.exports = router;
