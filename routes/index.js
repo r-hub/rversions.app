86404d1768d4dc8b40e8f504d86ea10d555436a0
Gábor Csárdi <csardi.gabor@gmail.com>
2021-07-01 09:52:27 +0200

    Legacy R versions: /r-oldrel/:n

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const endpoints = require('../lib/endpoints');
 const cache = require('../lib/cache');
 
+router.get("/r-oldrel/:n", async (req, res, next) => {
+    try {
+        const n = req.params.n;
+        if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'].indexOf(n) == -1) {
+            res.status(404)
+                .send({ error: 'End point not found, see documentation' });
+        }
+        const value = await cache.get('r-oldrel/' + n);
+          if (value === null) {
+              res.status(500)
+                  .send({ error: 'Internal server error, cannot find /r-oldrel/' + n });
+          } else {
+              res.type('application/json')
+                  .send(value);
+          }
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.get('/:what', async (req, res, next) => {
   try {
       const what = req.params.what;
